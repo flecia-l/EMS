db4caabe3b41b0c5468917e1342fe9f597ddd5da
2567eirc <eirc2567@gmail.com>
2024-04-10 17:08:06 +0800

    position change

--- Frontend/src/components/CommonTable_acc_ad_em.vue
+++ Frontend/src/components/CommonTable_acc_ad_em.vue
@@ -15,7 +15,7 @@
             <el-table-column label="操作" min-width="180">
                 <template slot-scope="scope">
                     <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
-                    <el-button size="mini" @click="handleDelete(scope.row)" type="danger">升职</el-button>
+                    <!--el-button size="mini" @click="handleDelete(scope.row)" type="danger">升职</el-button-->
                 </template>
             </el-table-column>
         </el-table>
